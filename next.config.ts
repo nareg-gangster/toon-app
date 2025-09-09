edb82c632b89ae35ae42ea9c4bf58668338ffd7d
nareg-gangster <naregj.nj@gmail.com>
2025-09-09 20:44:51 +0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  turbo: {
-    root: __dirname
-  }
-};
+  const nextConfig: NextConfig = {
+    /* config options here */
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+  };
 
-export default nextConfig;
+  export default nextConfig;
